f48c55fb9c18cf23a14858a8f5c5a2361eceb361
ali-moonlay <43127426+ali-moonlay@users.noreply.github.com>
2019-09-20 17:27:25 +0700

    ETL Fact Deal Tracking Stage

---
+++ DealTrackingStage/index.js
@@ -0,0 +1,19 @@
+let run = require('./run');
+
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    await run()
+        .then((result) => {
+            console.log('success');
+            context.res = {
+                body: JSON.stringify(result)
+            };
+        })
+        .catch((e) => {
+            context.res = {
+                body: JSON.stringify(e)
+            }
+        });
+};
