2ceaba15a71a3978e6b50958c9de1b31b2dae728
ali-moonlay <43127426+ali-moonlay@users.noreply.github.com>
2019-09-23 17:54:04 +0700

    ETL Timer Trigger (1)

---
+++ SpinningSCScheduler/index.js
@@ -0,0 +1,23 @@
+let run = require('../SpinningSC/run');
+
+module.exports = async function (context, myTimer) {
+    var timeStamp = new Date().toISOString();
+    
+    if (myTimer.IsPastDue)
+    {
+        context.log('JavaScript is running late!');
+    }
+    context.log('JavaScript timer trigger function ran!', timeStamp);   
+    await run()
+        .then((result) => {
+            console.log('success');
+            context.res = {
+                body: JSON.stringify(result)
+            };
+        })
+        .catch((e) => {
+            context.res = {
+                body: JSON.stringify(e)
+            }
+        });
+};
---
+++ WeavingSCTimer/index.js
@@ -0,0 +1,24 @@
+let run = require('../WeavingSC/run');
+
+
+module.exports = async function (context, myTimer) {
+    var timeStamp = new Date().toISOString();
+    
+    if (myTimer.IsPastDue)
+    {
+        context.log('JavaScript is running late!');
+    }
+    context.log('JavaScript timer trigger function ran!', timeStamp);   
+    await run()
+        .then((result) => {
+            console.log('success');
+            context.res = {
+                body: JSON.stringify(result)
+            };
+        })
+        .catch((e) => {
+            context.res = {
+                body: JSON.stringify(e)
+            }
+        });
+};
