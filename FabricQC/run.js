24f1d85f65bb6424f7ed4531f0b1ea872a052118
ali-moonlay <43127426+ali-moonlay@users.noreply.github.com>
2019-10-21 18:51:15 +0700

    Change to 100 / batch only

--- FabricQC/run.js
+++ FabricQC/run.js
@@ -72,7 +72,7 @@ const extractFQC = async function (times) {
         where lastmodifiedutc >= ?
         order by id
         offset 0 rows
-        fetch next 200 rows only`, {
+        fetch next 100 rows only`, {
             replacements: [timestamp],
             type: sqlFPConnection.sqlFP.QueryTypes.SELECT
         });
