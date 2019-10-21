5c74fe5589d6df3dc934eb5ecce43b23ba264281
ali-moonlay <43127426+ali-moonlay@users.noreply.github.com>
2019-10-21 18:31:16 +0700

    Update run.js

--- FabricQC/run.js
+++ FabricQC/run.js
@@ -72,7 +72,7 @@ const extractFQC = async function (times) {
         where lastmodifiedutc >= ?
         order by id
         offset 0 rows
-        fetch next 100 rows only`, {
+        fetch next 200 rows only`, {
             replacements: [timestamp],
             type: sqlFPConnection.sqlFP.QueryTypes.SELECT
         });
@@ -219,7 +219,7 @@ function load(data) {
                     .then((execResult) => {
                         t.commit()
                             .then(() => {
-                                resolve(results);
+                                resolve(execResult);
                             })
                             .catch((err) => {
                                 reject(err);
