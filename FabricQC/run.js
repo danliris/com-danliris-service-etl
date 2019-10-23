c1de7f30522ff4270d31c9cc31b3e32dea3b3cba
ali-moonlay <43127426+ali-moonlay@users.noreply.github.com>
2019-10-23 18:36:56 +0700

    450-475

--- FabricQC/run.js
+++ FabricQC/run.js
@@ -19,7 +19,7 @@ module.exports = async function () {
                 start: startedDate,
                 finish: finishedDate,
                 executionTime: spentTime + " minutes",
-                status: "Successful-30-Part16-" + dataCount
+                status: "Successful-25-Part17-" + dataCount
             };
             // return updateLog;
             return await updateMigrationLog(updateLog);
@@ -73,8 +73,8 @@ const extractFQC = async function (times) {
         color, construction, packingInstruction, uom, IsDeleted, IsUsed from fabricqualitycontrols
         where lastmodifiedutc >= ?
         order by id
-        offset 420 rows
-        fetch next 30 rows only`, {
+        offset 450 rows
+        fetch next 25 rows only`, {
             replacements: [timestamp],
             type: sqlFPConnection.sqlFP.QueryTypes.SELECT
         });
